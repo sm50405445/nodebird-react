4d5e511a5f69921986d38ee6f817b2f564d8cde4
sd50712321 <sd50712321@naver.com>
2020-05-08 00:28:23 +0900

    antd 적용

--- react-nodebird/ch1/front/pages/index.js
+++ react-nodebird/ch1/front/pages/index.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import Link from 'next/link'
+import Head from 'next/head'
+import AppLayout from '../components/AppLayout'
 
 const Home = () => {
     return(
-        <>
-            <Link href="/about"><a>about</a></Link>
-            <div>Hello, Next!</div>
+        <>  
+            <Head>
+                <title>NodeBird</title>
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/antd/4.2.0/antd.css"/>
+            </Head>
+            <AppLayout>
+                <Link href="/about"><a>about</a></Link>
+                <div>Hello, Next!</div>
+            </AppLayout>
         </>
         
     );
