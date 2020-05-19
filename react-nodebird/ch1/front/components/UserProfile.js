dbe461e2daed07b03ec7985f497c215b401970a3
sd50712321 <sd50712321@naver.com>
2020-05-20 00:48:52 +0900

    user profile 수정

--- react-nodebird/ch1/front/components/UserProfile.js
+++ react-nodebird/ch1/front/components/UserProfile.js
@@ -1,22 +1,28 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import {Menu,Input, Button,Row,Col,Card,Avatar,Form} from 'antd'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import { logoutAction } from '../reducers/user'
 
 const UserProfile = () => {
     const {user} = useSelector(state=>state.user)
+    const dispatch = useDispatch()
+    const onLogout = useCallback(()=>{
+        dispatch(logoutAction)
+    },[])
     return(
         <Card
-                        actions={[
-                            <div key="twit">짹짹 <br/>{user.Post.length}</div>,
-                            <div key="following">팔로잉 <br/>{user.Followings.length}</div>,
-                            <div key="follower">팔로워 <br/>{user.Followers.length}</div>,
-                        ]}
-                    >
-                        <Card.Meta
-                            avatar={<Avatar>{user.nickname[0]}</Avatar>}
-                            title={user.nickname}
-                        />
-                    </Card>
+            actions={[
+                <div key="twit">짹짹 <br/>{user.Post.length}</div>,
+                <div key="following">팔로잉 <br/>{user.Followings.length}</div>,
+                <div key="follower">팔로워 <br/>{user.Followers.length}</div>,
+            ]}
+        >
+            <Card.Meta
+                avatar={<Avatar>{user.nickname[0]}</Avatar>}
+                title={user.nickname}
+            />
+            <Button onClick={onLogout}>로그아웃</Button>
+        </Card>
     )
 }
 
